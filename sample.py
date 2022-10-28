dfe3e67334eac8bdd3aced33e7a7ec9199b86c32
Erwann Millon <erwann@Erwanns-MacBook-Air.local>
2022-10-29 01:14:56 +0200

    sample devvice fix

--- sample.py
+++ sample.py
@@ -67,22 +67,24 @@ def sample_plot_image(x_l, model, T=300):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("device = {device}")
     x_l = x_l.to(device)
-    img_size = x_l.shape[-1]
+    print(f"x_l.device = {x_l.device}")
     x_ab = torch.randn((1, 2, img_size, img_size), device=device)
-    img = torch.cat((x_l, x_ab), dim=1)
-    plt.figure(figsize=(15,15))
-    plt.axis('off')
-    num_images = 10
-    stepsize = int(T/num_images)
+    print(f"x_ab.shape = {x_ab.shape}")
+    img_size = x_l.shape[-1]
+    # img = torch.cat((x_l, x_ab), dim=1)
+    # plt.figure(figsize=(15,15))
+    # plt.axis('off')
+    # num_images = 10
+    # stepsize = int(T/num_images)
 
-    for i in range(0,T)[::-1]:
-        t = torch.full((1,), i, device=device, dtype=torch.long)
-        img = sample_timestep(img, t, model)
-        if i % stepsize == 0:
-            plt.subplot(1, num_images, i//stepsize+1)
-            show_lab_image(img.detach().cpu())
-            # show_tensor_image(img.detach().cpu())
-    plt.show()     
+    # for i in range(0,T)[::-1]:
+    #     t = torch.full((1,), i, device=device, dtype=torch.long)
+    #     img = sample_timestep(img, t, model)
+    #     if i % stepsize == 0:
+    #         plt.subplot(1, num_images, i//stepsize+1)
+    #         show_lab_image(img.detach().cpu())
+    #         # show_tensor_image(img.detach().cpu())
+    # plt.show()     
 
 # x = torch.randn((1, 1, 256, 256))
 if __name__ == "main":
