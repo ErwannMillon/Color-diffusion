fc29f6865463f8a2f1477ea65e9e7848d4ea208e
Erwann Millon <erwann@Erwanns-MacBook-Air.local>
2022-11-04 05:10:21 +0100

    modify train loop for modularity, to be able to add more parts to the model

--- train.py
+++ train.py
@@ -30,9 +30,10 @@ def optimize_diff(optim, model, batch, device,
     if (log):
         wandb.log({"epoch":e, "step":step, "loss":loss.item()})
 
-def train_model(model, train_dl, epochs, save_interval=15, 
-                display_every=200, T=300, batch_size=16,
-                log=True, device="cpu", ckpt=None, sample=True, writer=None):
+def train_model(model, train_dl, epochs, config, 
+                save_interval=15, display_every=200, 
+                log=True, ckpt=None, sample=True, writer=None):
+    device = config["device"]
     if ckpt:
         model.load_state_dict(torch.load(ckpt, map_location=device))
     model.train()
@@ -42,9 +43,13 @@ def train_model(model, train_dl, epochs, save_interval=15,
             real_L, real_AB = split_lab(batch[:1, ...].to(device))
             diff_loss = optimize_diff(optim_diff, model, batch, 
                                         device, config, step, e)
+
+            losses = dict(
+                diff_loss = diff_loss.item(),
+                )
         if e % save_interval == 0:
-            # print(f"epoch: {e}, loss {loss.item()}")
-            torch.save(model.state_dict(), f"./saved_models/model_{e}_.pt")
+            print(f"epoch: {e}, loss {losses}")
+            # torch.save(model.state_dict(), f"./saved_models/model_{e}_.pt")
             # for name, weight in model.named_parameters():
             #     writer.add_histogram(name,weight, e)
             #     writer.add_histogram(f'{name}.grad',weight.grad, e)
@@ -57,6 +62,7 @@ if __name__ == "__main__":
         batch_size = 1,
         img_size = 128,
         lr_unet = 1e-3,
+        device = get_device(),
         T = 300
     )
     writer = SummaryWriter('runs/colordiff')
@@ -70,8 +76,8 @@ if __name__ == "__main__":
     ckpt = None
     ic.disable()
 
-    train_model(diff_gen, train_dl, 150, batch_size=config["batch_size"], \
-                device=device, ckpt=ckpt, log=True, sample=True,\
+    train_model(diff_gen, train_dl, 150,
+                ckpt=ckpt, log=True, sample=True,
                 save_interval=10, writer=writer, config=config)
 ############
 # def get_loss(model, x_0, t):
