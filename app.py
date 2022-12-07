ef2239ba90996d2de9dabbefe8a93c6588ad7f6f
Erwann Millon <erwann@Erwanns-MacBook-Air.local>
2022-12-07 20:20:46 +0100

    app

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import gradio as gr
 from transformers import pipeline
 from PLModel import PLColorDiff
-from autoencoder import GreyscaleAutoEnc
 from dataset import ColorizationDataset
-from stable_diffusion.model.unet import UNetModel
 from utils import lab_to_rgb, split_lab
 import torch
 import default_configs
@@ -18,7 +16,7 @@ def get_image(image):
     # x_l, _ = split_lab(batch)
     # bw = torch.cat((x_l, *[torch.zeros_like(x_l)] * 2), dim=1)
     model.eval()
-    img = model.sample_plot_image(batch, show=False, prog=True)
+    img = model.sample_plot_image(batch)
     rgb_img = lab_to_rgb(*split_lab(img))
     # model.test_step(batch)
     return(rgb_img[0])
@@ -38,17 +36,12 @@ conf = SimpleUnetConfig = dict (
     epochs=100,
     using_cond=False
 )
-unet_config = default_configs.celeba_unet_config
-enc_config = default_configs.celeba_encoder_conf
-colordiff_config = default_configs.celeba_colordiff_config
-colordiff_config["device"]= "mps"
-ckpt_path = "./saved_models/endofepochceleb_64px.ckpt"
+ckpt_path = "checkpoints/epoch=1-step=706.ckpt"
 ckpt = torch.load(ckpt_path, map_location=torch.device("mps"))
-autoenc = GreyscaleAutoEnc(enc_config, None)
-unet = UNetModel(**unet_config)
-model = PLColorDiff(unet, None, None, autoenc, **colordiff_config)
-model.load_state_dict(ckpt["state_dict"])
+unet = SimpleUnet()
+model = PLColorDiff(unet, None, None)
 ic.disable()
+model.load_state_dict(ckpt["state_dict"])
 
 demo = gr.Interface(
     get_image,
--- default_configs.py
+++ default_configs.py
@@ -11,42 +11,6 @@ encoder_conf = dict(
     z_channels=256
 )
 
-celeba_encoder_conf = dict(
-  in_channels=1,
-  channels=128,
-  channel_multipliers=[1, 1, 2, 3],
-  n_resnet_blocks=2,
-  z_channels=256
-)
-
-celeba_unet_config = dict(
-    in_channels=3,
-    out_channels=2,
-    channels=128,
-    attention_levels=[1, 2],
-    n_res_blocks=2,
-    channel_multipliers=[1, 2, 2, 3],
-    n_heads=2,
-    tf_layers=1,
-    d_cond=256
-)
-
-celeba_colordiff_config = dict(
-    device = "gpu",
-    pin_memory = True,
-    T=350,
-    lr=4e-5,
-    batch_size= 8,
-    img_size = 64,
-    sample=True,
-    should_log=True,
-    epochs=4,
-    using_cond=True,
-    display_every=200,
-    dynamic_threshold=False,
-    train_autoenc=False,
-) 
-
 SimpleUnetConfig = dict (
     device = get_device(),
     pin_memory = torch.cuda.is_available(),
@@ -81,7 +45,7 @@ ColorDiffConfig = dict(
     lr=5e-4,
     batch_size=1,
     img_size = 64,
-    sample=True,
+    sample=False,
     should_log=False,
     val_every=20,
     epochs=100,
